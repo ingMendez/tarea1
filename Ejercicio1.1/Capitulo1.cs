822635a79daac0c87a43d5e9e5fb3087533f9bf4
Reydi Alejandro Mendez Suarez <raydi_mendez@ucne.edu.do>
2018-09-01 17:38:44 -0400

    termindo

--- Ejercicio1.1/Capitulo1.cs
+++ Ejercicio1.1/Capitulo1.cs
@@ -9,7 +9,26 @@ namespace Ejercicio1._1
    class Capitulo1
     {
     
-     
+     public void menu()
+        {
+            Console.WriteLine("*********************menu*****************************");
+            Console.WriteLine("*---capitulo1----------------------------------------*");
+            Console.WriteLine("* 1   agregrar nombre1.1:                            *");
+            Console.WriteLine("* 2   agregar mas mensajes en la aplicacion_1.5      *");
+            Console.WriteLine("*---capitulo2:---------------------------------------*");
+            Console.WriteLine("*3    calculo de cualquier poligono_2.1:             *");
+            Console.WriteLine("*4    Converciond e grados a Radianes_2.3:           *");
+            Console.WriteLine("*5    Convercion de dolares a Euros y viseversa_2.5: *");
+            Console.WriteLine("*---capitulo3----------------------------------------*");
+            Console.WriteLine("*6    Numeros pares o impares_3.1:                   *");
+            Console.WriteLine("*7    numeracion de los dias de la semana_3.4:       *");
+            Console.WriteLine("*8    calculo de poligono_1.5:                       *");
+            Console.WriteLine("*---capitulo4----------------------------------------*");
+            Console.WriteLine("*9     tabla de multiplicacion_4.1:                  *");
+            Console.WriteLine("*10    Elevar numeros acualquier potencia_4.2:       *");
+            Console.WriteLine("*11    Promedio de edad_4.5:                         *");
+
+        }
         public void ejercicio1()
         {
             Console.WriteLine("reydi alejandro");
--- Ejercicio1.1/Program.cs
+++ Ejercicio1.1/Program.cs
@@ -10,79 +10,74 @@ namespace Ejercicio1._1
     {
         static void Main(string[] args)
         {
+            int n;
             int Opcion;
             Capitulo1 p1 = new Capitulo1();
             capitulo2 p2 = new capitulo2();
             Capitulo3 p3 = new Capitulo3();
             Capitulo4 p4 = new Capitulo4();
 
-            Console.WriteLine("*********************menu*****************************");
-            Console.WriteLine("*---capitulo1----------------------------------------*");
-            Console.WriteLine("* 1   Tabla de multipliacion1.1:                     *");
-            Console.WriteLine("* 2   _1.5:                                          *");
-            Console.WriteLine("*---capitulo2:---------------------------------------*");
-            Console.WriteLine("*3    calculo de cualquier poligono_2.1:             *");
-            Console.WriteLine("*4    Converciond e grados a Radianes_2.3:           *");
-            Console.WriteLine("*5    Convercion de dolares a Euros y viseversa_2.5: *");
-            Console.WriteLine("*---capitulo3----------------------------------------*");
-            Console.WriteLine("*6    Numeros pares o impares_3.1:                   *");
-            Console.WriteLine("*7    numeracion de los dias de la semana_3.4:       *");
-            Console.WriteLine("*8    calculo de poligono_1.5:                       *");
-            Console.WriteLine("*---capitulo4----------------------------------------*");
-            Console.WriteLine("*9     tabla de multiplicacion_4.1:                  *");
-            Console.WriteLine("*10    Elevar numeros acualquier potencia_4.2:       *");
-            Console.WriteLine("*11    Promedio de edad_4.5:                         *");
-
+            p1.menu();
             Opcion = Convert.ToInt32(Console.ReadLine());
-
-            switch (Opcion)
+            do
             {
-                case 1:
-                   
-                    p1.ejercicio1();
-                    Console.ReadKey();
-                    break;
-                case 2: p1.ejercicio5();
-                    Console.ReadKey();
-                    break;
-                case 3:
-                    p2.ejercicios1();
-                    Console.ReadKey();
-                    break;
-                case 4:
-                    p2.Ejercicos3()
-                    Console.ReadKey();
-                    break;
-                case 5:
-                    p2.ejercicios5();
-                    Console.ReadKey();
-                    break;
-                case 6:
-                    p3.ejercicio3_1();
-                    Console.ReadKey();
-                    break;
-                case 7:
-                    p3.Ejercicios3_4();
-                    Console.ReadKey();
-                    break;
-                case 8:
-                    p3.ejercicio3_5();
-                    break;
-                case 9:
-                    p4.Ejercicio4_1();
-                    Console.ReadKey();
-                    break;
-                case 10:
-                    p4.ejercicios4_2();
-                    Console.ReadKey();
-                    break;
-                case 11:
-                    p4.ejercicios4_5();
-                    Console.ReadKey();
-                    break;
-               
+                switch (Opcion)
+                {
+                    case 1:
 
-            }
+                        p1.ejercicio1();
+                        Console.ReadKey();
+
+                        break;
+                    case 2:
+                        p1.menu();
+                        p1.ejercicio5();
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        p2.ejercicios1();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        p2.Ejercicos3();
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        p2.ejercicios5();
+                        Console.ReadKey();
+                        break;
+                    case 6:
+                        p3.ejercicio3_1();
+                        Console.ReadKey();
+                        break;
+                    case 7:
+                        p3.Ejercicios3_4();
+                        Console.ReadKey();
+                        break;
+                    case 8:
+                        p3.ejercicio3_5();
+                        break;
+                    case 9:
+                        p4.Ejercicio4_1();
+                        Console.ReadKey();
+                        break;
+                    case 10:
+                        p4.ejercicios4_2();
+                        Console.ReadKey();
+                        break;
+                    case 11:
+                        p4.ejercicios4_5();
+                        Console.ReadKey();
+                        break;
+
+                   // case 12: default: break;
+                }
+              
+
+                Console.WriteLine("desea salir del programa ponga 1");
+                n = Convert.ToInt32(Console.ReadLine());
+
+            } while(n !=1);
 
         }
     }
